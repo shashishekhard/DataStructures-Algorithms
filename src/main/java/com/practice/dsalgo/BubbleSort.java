299d3bdc64c58b8af548e46058f2461b2bdced17
shashishekhard <shashishekhar.deshpande@gmail.com>
2017-01-31 23:21:00 -0500

    updated bubble sort example
    Change-Id: Ifcec1f68de67d27d27ebd876b51245d0f39308ea

--- src/main/java/com/practice/dsalgo/BubbleSort.java
+++ src/main/java/com/practice/dsalgo/BubbleSort.java
@@ -13,8 +13,31 @@ public class BubbleSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = {10,34,23,19,87,56,78,43,99,5};
+		
+		BubbleSort.print(BubbleSort.bubbleSort(arr));
 
+	}
+	
+	public static int[] bubbleSort(int[] arr){
+		int temp;
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j] > arr[j+1]){
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		
+		return arr;
+	}
+	
+	public static void print(int[] arr){
+		for(int i=0;i<arr.length;i++){
+			System.out.println(arr[i]+" ");
+		}
 	}
 
 }
