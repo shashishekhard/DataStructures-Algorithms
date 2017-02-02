ca8a9eed858840729c729b34125ebe4365e76f8c
Shashishekhar Deshpande <sshekhar.deshpande@gmail.com>
2017-02-01 23:11:11 -0500

    Updated bubble sort code
    Adding description and logic in comments.

--- src/main/java/com/practice/dsalgo/sorting/BubbleSort.java
+++ src/main/java/com/practice/dsalgo/sorting/BubbleSort.java
@@ -4,8 +4,13 @@
 package com.practice.dsalgo.sorting;
 
 /**
- * @author shash
+ * @author shashishekhar
  *
+ * @description 
+ * Bubble sort is a simplest sorting algorithm also the most inefficient one.
+ * The idea here is to iterate the array sequentially several times where each element is compared 
+ * with its successor.
+ * i.e. x[i] > x[i+1] -> swap(x[i],x[i+1]) 
  */
 public class BubbleSort {
 
@@ -13,12 +18,23 @@ public class BubbleSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Hardcoded array for testing.
 		int[] arr = {10,34,23,19,87,56,78,43,99,5};
 		
+		//We can add the scanner and get elements from the user.
+		//Scanner scanner = new Scanner(System.in);
+		//int n = scanner.nextInt();
+		
+		//for(int i=0;i<n;i++){
+		//	arr[i] = scanner.nextInt();
+		//}
+		// The scanner has to be closed after the values are taken from the user
+		/* scanner.close()*/
 		BubbleSort.print(BubbleSort.bubbleSort(arr));
 
 	}
 	
+	//bubble sort code to compare and swap elements.
 	public static int[] bubbleSort(int[] arr){
 		int temp;
 		for(int i=0;i<arr.length;i++){
